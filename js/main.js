01d51432bb70f646a3d6cee0fc9cd7535b311fb9
Peter Kalavritinos <peterkalav1994@gmail.com>
2021-12-06 14:02:14 -0500

    Transfer from sandbox to github

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/*var numOfDrinks = 5;
+
+var drinks = function () {
+  var tea = 6 + numOfDrinks;
+  console.log(tea);
+};
+
+drinks();*/
+
+/*if (numOfDrinks === 5) {
+  var soda = "lemon-lime";
+  console.log(soda);
+};*/
+
+/*if (numOfDrinks === 5) {
+  let soda = "lemon-lime";
+  console.log(soda);
+};*/
+/*console.log(soda);*/
+
+const numOfDrinks = 5;
+const drinks = function() {
+    const tea = 6 + numOfDrinks;
+    console.log(tea);
+};
+
+drinks();
